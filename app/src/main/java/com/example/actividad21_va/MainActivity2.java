2c49f376f637442eec748d479e86441eb4a5f4d1
E8Vitel <viktoralvz@gmail.com>
2023-10-03 16:03:01 -0300

    Actividad 2.1 Realizada

--- app/src/main/java/com/example/actividad21_va/MainActivity2.java
+++ app/src/main/java/com/example/actividad21_va/MainActivity2.java
@@ -11,6 +11,5 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
     }
 }
